67f26758220e5885ea74f95d5f4e0205c449914d
Dotan Alter <dotan@stream.security>
2025-06-08 12:04:33 +0300

    first commit to add resources to automated integration

---
+++ automated-integration/main.tf
@@ -0,0 +1,59 @@
+provider "google" {
+  project = var.google_project_id
+  region  = var.google_region
+}
+
+resource "google_pubsub_topic" "project_events" {
+  name    = var.pubsub_topic_name
+  project = var.google_project_id
+}
+
+resource "google_logging_organization_sink" "project_create" {
+  name             = var.log_sink_name
+  org_id           = var.org_id
+  destination      = "pubsub.googleapis.com/projects/${var.google_project_id}/topics/${google_pubsub_topic.project_events.name}"
+  filter           = "protoPayload.methodName=\"google.cloud.resourcemanager.v1.Projects.CreateProject\""
+  include_children = true
+}
+
+resource "google_pubsub_topic_iam_binding" "allow_sink_publish" {
+  topic   = google_pubsub_topic.project_events.name
+  role    = "roles/pubsub.publisher"
+  members = [google_logging_organization_sink.project_create.writer_identity]
+  project = var.google_project_id
+}
+
+# Placeholder for function source upload
+# resource "google_storage_bucket_object" "function_zip" { ... }
+
+resource "google_cloudfunctions2_function" "handle_project_create" {
+  name     = var.function_name
+  location = var.google_region
+  project  = var.google_project_id
+  build_config {
+    runtime     = var.function_runtime
+    entry_point = var.function_entry_point
+    source {
+      storage_source {
+        bucket = var.function_source_bucket
+        object = var.function_source_object
+      }
+    }
+  }
+  service_config {
+    timeout_seconds  = var.function_timeout
+    ingress_settings = var.ingress_settings
+  }
+  event_trigger {
+    trigger_region = var.google_region
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.project_events.id
+    retry_policy   = var.retry_policy
+  }
+}
+
+resource "google_project_iam_member" "function_pubsub_subscriber" {
+  project = var.google_project_id
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:${google_cloudfunctions2_function.handle_project_create.service_config[0].service_account_email}"
+}
---
+++ automated-integration/variables.tf
@@ -0,0 +1,74 @@
+variable "google_project_id" {
+  description = "The Google Project ID to use for the integration."
+  type        = string
+}
+
+variable "google_region" {
+  description = "The Google Region to use for the integration."
+  type        = string
+}
+
+variable "org_id" {
+  description = "The Organization ID to use for the log sink."
+  type        = string
+}
+
+variable "function_source_bucket" {
+  description = "The name of the bucket containing the Cloud Function source code."
+  type        = string
+  default     = "my-function-source-bucket"
+}
+
+variable "function_source_object" {
+  description = "The name of the zip file (object) containing the Cloud Function source code."
+  type        = string
+  default     = "function-source.zip"
+}
+
+variable "pubsub_topic_name" {
+  description = "The name of the PubSub topic for project create events."
+  type        = string
+  default     = "stream-security-automated-integration"
+}
+
+variable "log_sink_name" {
+  description = "The name of the log sink for project create events."
+  type        = string
+  default     = "stream-security-automated-integration"
+}
+
+variable "function_name" {
+  description = "The name of the Cloud Function handling project create events."
+  type        = string
+  default     = "stream-security-automated-integration"
+}
+
+variable "function_runtime" {
+  description = "The runtime for the Cloud Function (e.g., python310, nodejs20, etc.)."
+  type        = string
+  default     = "python310"
+}
+
+variable "function_entry_point" {
+  description = "The entry point for the Cloud Function."
+  type        = string
+  default     = "main"
+}
+
+variable "function_timeout" {
+  description = "Timeout (in seconds) for the Cloud Function."
+  type        = number
+  default     = 60
+}
+
+variable "ingress_settings" {
+  description = "Ingress settings for the Cloud Function (e.g., ALLOW_INTERNAL_ONLY, ALLOW_ALL, etc.)."
+  type        = string
+  default     = "ALLOW_INTERNAL_ONLY"
+}
+
+variable "retry_policy" {
+  description = "Retry policy for the Cloud Function event trigger (e.g., RETRY_POLICY_RETRY, RETRY_POLICY_DO_NOT_RETRY)."
+  type        = string
+  default     = "RETRY_POLICY_RETRY"
+}
